062d43482682828cb9062b50974310e707cb09bf
Mark Mossberg <mark.mossberg@gmail.com>
2014-11-12 16:37:33 -0500

    Rename obscure inputs

--- mux_2x1.v
+++ mux_2x1.v
@@ -8,18 +8,18 @@
 
 `timescale 1ns / 1ns
 
-module mux_2x1 (a, b, sel, out);
+module mux_2x1 (input_a, input_b, sel, out);
 
-input [31:0] a, b;
+input [31:0] input_a, input_b;
 input sel;
 output reg [31:0] out;
 
-always @(sel or a or b )
+always @(sel or input_a or input_b )
 begin
     if (sel)
-        out <= b;
+        out <= input_b;
     else
-        out <= a;
+        out <= input_a;
 end
 
 endmodule
