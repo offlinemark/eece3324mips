e5b7199f88ba2162689c71d9f4f9134e9f90e719
Mark Mossberg <mark.mossberg@gmail.com>
2014-11-12 21:24:07 -0500

    Finish #1 tb

--- mux_2x1_tb.v
+++ mux_2x1_tb.v
@@ -14,31 +14,48 @@ reg [31:0] input_a, input_b;
 reg sel;
 wire [31:0] out; 
 
+reg clk;
+
 mux_2x1 mux (input_a, input_b, sel, out);
 
+/* Clock */
+always
+    #10 clk = ~clk;
+
+/* Display mux state */
+always @(input_a or input_b or sel)
+    $display("%t:\n\tsel\t%b\n\tinput_a\t%b\n\tinput_b\t%b\n\tout\t%b\n",
+             $time, sel, input_a, input_b, out);
+
+/* Testbench */
 initial begin
+    $timeformat(-9, 0, " ns", 2);
+    clk = 0;
     input_a = 0;
     input_b = 0;
     sel = 0;
-    $display("Output Q: %b", out);
 
-    #10
+    @(posedge clk)
     input_a = 1;
     input_b = 0;
     sel = 0;
-    $display("Output Q: %b", out);
 
-    #10
+    @(posedge clk)
     input_a = 0;
     input_b = 0;
     sel = 0;
-    $display("Output Q: %b", out);
 
-    #10
+    @(posedge clk)
     input_a = 0;
     input_b = 1;
     sel = 1;
-    $display("Output Q: %b", out);
+
+    @(posedge clk)
+    input_a = 0;
+    input_b = 0;
+    sel = 0;
+
+    $finish;
 end
 
 endmodule
