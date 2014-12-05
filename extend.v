100a658b7c0509f57caa7f48f3eefb2af04d4149
Mark Mossberg <mark.mossberg@gmail.com>
2014-12-05 01:10:17 -0500

    Separate non cpu testbenches into own dir

--- extend.v
+++
@@ -1,8 +0,0 @@
-module extend (in, out);
-
-input [15:0] in;
-output [31:0] out;
-
-assign out = {{16{in[15]}}, in[15:0]};
-
-endmodule
--- shift_left2.v
+++
@@ -1,9 +0,0 @@
-module shift_left2 (in, out);
-
-input [31:0] in;
-output reg [31:0] out;
-
-always @(in)
-    out = in << 2;
-    
-endmodule
